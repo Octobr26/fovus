ca8c9d6777033a452117f891450cee7e367ee4f0
luis diaz <luis.diaz@intellimind.com>
2024-05-01 00:35:43 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MainLayout from "./layout/MainLayout";
+import { Home } from "./pages/Home";
+
+function App() {
+    return (
+        <>
+            <MainLayout>
+                <Home />
+            </MainLayout>
+        </>
+    );
+}
+
+export default App;
