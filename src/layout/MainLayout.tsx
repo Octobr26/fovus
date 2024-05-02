5657edd2da0e528e3a2cde40ff20aa5c048e7be9
luis diaz <luis.diaz@intellimind.com>
2024-05-01 23:11:42 -0700

    added bg color

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Header } from "./Header";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div className="main-layout">
+        <div className="main-layout dark:bg-gray-900">
             <Header />
 
             <div>{children}</div>
